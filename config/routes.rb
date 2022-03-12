725f3593481277b1458663719468a347a52898e3
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-12 13:29:12 -0500

    Generate Posts model, controller, route, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-
+  root "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:new, :create]
-  # resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index]
   # Defines the root path route ("/")
-  root to: 'devise/registrations#new'
 end
