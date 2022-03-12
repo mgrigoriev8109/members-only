bc5464540933712a0b77097afc64ee02ba3e7f14
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-12 16:18:14 -0500

    Add post controller actions
    Along with before_action authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,25 @@
 class PostsController < ApplicationController
+
   def index
   end
 
+  before_action :authenticate_user!
   def new
+    @post = Post.new
   end
 
+  before_action :authenticate_user!
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
