1ebae012f9deecfa65457a936139ff11d90dad26
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-12 10:25:53 -0500

    Begin generating User model
    Create User validations, routes for #new #create actions, and update schema to include :username and :id

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:new, :create]
+  resources :posts, only: [:new, :create, :index]
   # Defines the root path route ("/")
-  root "posts#index"
+  root "users#index"
 end
