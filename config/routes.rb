978af39e15475fd3a648f6058dffab2d60ec5ed0
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-12 13:43:14 -0500

    Fix sign_out
    Changing routes to be devise and modifying initializer for sign_out to be GET

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:new, :create]
   resources :posts, only: [:new, :create, :index]
   # Defines the root path route ("/")
 end
