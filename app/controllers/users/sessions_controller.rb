f06364e39745605764f3d1de81d9aa230023d635
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-16 15:35:05 -0400

    Begin work on sign-in error flash messages

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,13 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+   def create
+    super
+
+    unless resource.save
+      render :new, status:unprocessable_entity
+    end
+   end
 
   # DELETE /resource/sign_out
   # def destroy
