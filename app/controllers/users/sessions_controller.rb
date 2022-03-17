b7ac2c2b7b0e0d3cc65493ec626575cc16e370d3
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-16 21:27:54 -0400

    Change sign-in controller to source code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,7 +12,6 @@ class Users::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
    def create
     self.resource = warden.authenticate!(auth_options)
-    debugger
     set_flash_message!(:notice, :signed_in, :alert, :error)
     sign_in(resource_name, resource)
     yield resource if block_given?
